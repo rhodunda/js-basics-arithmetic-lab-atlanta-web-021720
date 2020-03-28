70dfde3276227430d1319edb38d7c68a2a7237d9
Bryan Rhodunda <rhodunda@udel.edu>
2020-03-28 17:11:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here
+
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = 16 % 6
+const max = Math.max(5, 10, 7, 20)
